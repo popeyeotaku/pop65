8837d738137576bad8d31b43a4413eff0a9e7ad8
Troy Fullwood <everyfan3417@gmail.com>
2024-09-10 11:09:51 -0400

    added demo for label comments in test.s

--- test.s
+++ test.s
@@ -1,26 +1,30 @@
         .org 0
         .dbg "al ${V} .{L}"
+; C64 PRG starting address
         .word $801
+; BASIC programs load-in here
         .org $801
+; C64 BASIC program to start main program.
+; (bottom of program)
 bashed  .word bashe1
         .word 10
         .byte $9E,'2061',0
 bashe1  .word 0
-        jmp start
-chrout  = $ffd2
-point1  = $fb
-msg     .byte 13,13,'HELLO WORLD',13,13,0
-start   lda #<msg
+        jmp start                               ; put your init code at `start`
+chrout  = $ffd2                                 ; C64 putchar
+point1  = $fb                                   ; zero-page pointer
+msg     .byte 13,13,'HELLO WORLD',13,13,0       ; 'hello' message string
+start   lda #<msg                               ; print the message and iloop
         sta point1
         lda #>msg
         sta point1+1
         jsr print
         jmp *
-print   ldy #0
+print   ldy #0                                  ; print the string at (point1)
 print1  lda (point1),y
         beq print2
         jsr chrout
         iny
         bne print1
 print2  rts
-end
+end                                             ; (top of program)
